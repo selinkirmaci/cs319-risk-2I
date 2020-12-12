48ca7bee196adf90c94e820b42a2ccd6fc75a078
kaan <akaanug@gmail.com>
2020-12-12 21:03:35 +0300

    added property "cursed"

--- backend/src/main/java/backend/Troop.java
+++ backend/src/main/java/backend/Troop.java
@@ -7,8 +7,17 @@ package backend;
 public class Troop {
 
     protected int value;
+    protected boolean cursed = false;
     
     public int getValue() {
         return value;
+    }
+
+    public void curse() {
+        cursed = true;
+    }
+
+    public boolean isCursed() {
+        return cursed;
     }
 }
