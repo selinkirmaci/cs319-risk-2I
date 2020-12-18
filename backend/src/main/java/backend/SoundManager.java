084a10b7d662c67c4b40ef1e7c9eaf4a39c81db8
selinkirmaci <selinkirmaci08@gmail.com>
2020-12-18 23:06:42 +0300

    wrote sound manager

--- backend/src/main/java/backend/SoundManager.java
+++ backend/src/main/java/backend/SoundManager.java
@@ -1,4 +1,38 @@
 package backend;
 
-public class SoundManager {
+public class SoundManager
+{
+    private int volume;
+    private boolean soundOn;
+    private boolean musicOn;
+    public SoundManager(int volume,boolean soundOn,boolean musicOn)
+    {
+        this.musicOn = musicOn;
+        this.soundOn = soundOn;
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isMusicOn() {
+        return musicOn;
+    }
+
+    public void setMusicOn(boolean musicOn) {
+        this.musicOn = musicOn;
+    }
+
+    public boolean isSoundOn() {
+        return soundOn;
+    }
+
+    public void setSoundOn(boolean soundOn) {
+        this.soundOn = soundOn;
+    }
 }
